03ff9f4763d1e8980780ae416c444fbe5af349c0
moedeera <82620264+moedeera@users.noreply.github.com>
2021-10-04 16:24:32 -0600

    update

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ app.use(express.urlencoded({extended: false}))
 // Page Directory
 
 // app.use('/', require('./routes/welcome'))
-// app.use('/Portfolio', require('./routes/portfolio'))
-app.use('/Projects', require('./routes/projects'))
+app.use('/Portfolio', require('./routes/api/portfolio'))
+app.use('/Projects', require('./routes/api/projects'))
 app.use('/', require('./routes/dashboard'))
 
 
--- routes/api/projects.js
+++ routes/api/projects.js
@@ -2,9 +2,9 @@ const express = require('express')
 const router = express.Router()
 const bcrypt = require ('bcrypt')
 const mongoose = require ('mongoose')
-const User = require ('../models/Users.js')
-const Profile = require ('../models/Profile.js')
-const List = require ('../models/List.js')
+const User = require ('../../models/Users.js')
+const Profile = require ('../../models/Profile.js')
+const List = require ('../../models/List.js')
 
 // Portfolio Page
 router.get(('/'), (req, res)=> res.render('Projects/Projects.ejs'))
