ecd987e24aa7fb3596f2441e7f10b5554446632d
moedeera <82620264+moedeera@users.noreply.github.com>
2021-10-13 20:14:00 -0600

    commit

--- public/assets/Portfolio/Chess/Chess.js
+++ public/assets/Portfolio/Chess/Chess.js
@@ -7,6 +7,9 @@ var k = 2;
 var k0 =20;
 
 
+var state = 0;
+var prev;
+
 var Map = [
 
 8,6,4,k,q,4,6,8,
@@ -81,16 +84,20 @@ function Mapper (){
                 }
 
 if (Map[j]===6){  
-    boxes[j].innerHTML ="&#9816;";                
+    boxes[j].innerHTML ="&#9816;";  
+    boxes[j].style.color ="white";                 
          }                                         
     if (Map[j]===4){                        
-         boxes[j].innerHTML ="&#9815;";                       
+         boxes[j].innerHTML ="&#9815;";
+         boxes[j].style.color ="white";                          
             }                                       
          if (Map[j]===k){                               
-            boxes[j].innerHTML ="&#9813;";                                
+            boxes[j].innerHTML ="&#9813;";
+            boxes[j].style.color ="white";                                   
             }                                             
             if (Map[j]===q){                                        
-                boxes[j].innerHTML ="&#9812;";    
+                boxes[j].innerHTML ="&#9812;";
+                boxes[j].style.color ="white";       
             }
                 if (Map[j]===80){                                                        
                     boxes[j].innerHTML ="&#9820;";  
@@ -121,19 +128,43 @@ if (Map[j]===6){
 
 function PlayGame(e){
 
-for(var j=0; j<64; j++){
-    
-if(e.target===boxes[j]){
-boxes[j].style.background = "white"
+  for(var j=0; j<64; j++){
+   
 
+         if(e.target===boxes[j]&& Map[j]!==0){
+
+             if(state===0){
+                
+                  boxes[j].style.opacity = "0.3"
+                  prev = j;
+                  state = 1;
+             } else if (state===1){
+                Clear()
+                boxes[j].innerHTML = boxes[prev].innerHTML
+                boxes[prev].innerHTML = "";
+                Map[j]=Map[prev]
+                Map[prev]=0;
+                console.log(Map)
+                Mapper()
+                state = 0;
+
+             }
+
+
+           }
+
+
+    } 
 
 }
 
+
+function Clear (){
+    for (var j=0; j<64; j++){
+        boxes[j].style.opacity = "1"
+
+    }
 }
-
-}
-
-
                                  
                                
   
