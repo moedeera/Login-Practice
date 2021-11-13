8df0fc347e1b79568cd450fe8aa11c29422e51bb
moedeera <82620264+moedeera@users.noreply.github.com>
2021-11-13 09:42:34 -0600

    commit

--- public/assets/main.js
+++ public/assets/main.js
@@ -2,7 +2,7 @@ const main = document.getElementById("main")
 const account = document.getElementById("account")
 const settings = document.getElementById("settings")
 const nav = document.querySelector(".sideNav")
-
+var prev =  0;
 
 nav.addEventListener('click', (e)=>{
 
@@ -35,17 +35,22 @@ document.getElementById("close").addEventListener('click',(e)=>{
     document.getElementById("close").classList.add("no-show")
     document.getElementById("progress").classList.add("no-show")
     document.getElementById("Appointment").classList.add("no-show")
-
+    document.getElementById("mainOpt").style.opacity ='1';
+   
 })
 
 
 document.getElementById("Progress").addEventListener('click', (e)=>{
+    document.getElementById("mainOpt").style.opacity ='0.1'
+   
     document.getElementById("close").classList.remove("no-show")
     document.getElementById("progress").classList.remove("no-show")
 
 })
 
 document.getElementById("Appt").addEventListener('click', (e)=>{
+    document.getElementById("mainOpt").style.opacity ='0.1'
+  
     document.getElementById("close").classList.remove("no-show")
     document.getElementById("Appointment").classList.remove("no-show")
 
