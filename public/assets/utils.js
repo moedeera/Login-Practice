2a420ceda06fe02704fdab95844253e4bb67187e
moedeera <82620264+moedeera@users.noreply.github.com>
2022-02-17 03:16:11 -0600

    commit

--- public/assets/utils.js
+++ public/assets/utils.js
@@ -27,17 +27,3 @@ ExitBtn2.addEventListener("click", () => {
   dashboard.classList.remove("no-show");
 });
 
-const PublicBtn = document.getElementById("Public");
-const SettingsBtn = document.getElementById("Settings");
-const DeleteBtn = document.getElementById("Delete");
-
-PublicBtn.addEventListener("click", () => {
-  console.log("you clicked public button");
-});
-
-SettingsBtn.addEventListener("click", () => {
-  console.log("you clicked settings button");
-});
-DeleteBtn.addEventListener("click", () => {
-  console.log("you clicked public button");
-});
