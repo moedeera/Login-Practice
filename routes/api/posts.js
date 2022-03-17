e9bf6e63de0b8b0de1eab9e040e1715d2c0a3ead
moedeera <82620264+moedeera@users.noreply.github.com>
2022-03-17 10:43:46 -0600

    committ

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const mongoose = require("mongoose");
+const { Profiles } = require("./Database");
 
 router.get("/", (req, res) => {
-  res.send("hello");
+  res.send(Profiles);
 });
 
 module.exports = router;
