caddb9bce279c051918c80e8e6a80ea6c21435e7
moedeera <82620264+moedeera@users.noreply.github.com>
2022-03-17 09:53:28 -0600

    committ

--- app.js
+++ app.js
@@ -5,6 +5,19 @@ const mongoose = require("mongoose");
 const path = require("path");
 const morgan = require("morgan");
 const session = require("express-session");
+const cors = require("cors");
+app.use(cors());
+
+app.use(function (req, res, next) {
+  //Enabling CORS
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization"
+  );
+  next();
+});
 
 const flash = require("express-flash");
 const { userJoin, getCurrentUser, getGuest, getHost } = require("./utils/user");
